0432ad75f66158d9fe16a05c2cc15ff168d1b47f
Leo Yu Ho, Lo <leoyuholo@gmail.com>
2021-12-28 16:56:17 +0800

    check if exist before creating symbolic links

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ fi
 
 nb_dir=${HOME}/jupyter
 mkdir $nb_dir
-ln -s /data ${nb_dir}/data
-ln -s /demo ${nb_dir}/demo
+test -d ${nb_dir}/data || ln -s /data ${nb_dir}/data
+test -d ${nb_dir}/demo || ln -s /demo ${nb_dir}/demo
 
 USERID=$(id -u) GROUPID=$(id -g) docker-compose -p ${USER}-gpu up -d
 docker-compose -p ${USER}-gpu exec jupyter /bin/bash -c 'cd /jupyter && /bin/bash /scripts/run.sh'
